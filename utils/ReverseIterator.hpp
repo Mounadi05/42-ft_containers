3a549ab9f52327eb94aabc398603fa4bdc3aff2e
Mounadi05 <mounadi2015@gmail.com>
2023-03-08 05:40:17 +0100

    upadte ReverseIterator

--- utils/ReverseIterator.hpp
+++ utils/ReverseIterator.hpp
@@ -6,19 +6,17 @@
 namespace ft
 {
 template <class Iterator>
-class reverse_iterator : public
-    iterator<typename iterator_traits<Iterator>::iterator_category,
-    typename iterator_traits<Iterator>::value_type,
-    typename iterator_traits<Iterator>::difference_type,
-    typename iterator_traits<Iterator>::pointer,
-    typename iterator_traits<Iterator>::reference> {
-    protected:
-        Iterator current;
-    public:
-        typedef Iterator                                                iterator_type;
-        typedef typename iterator_traits<Iterator>::difference_type     difference_type;
-        typedef typename iterator_traits<Iterator>::reference           reference;
-        typedef typename iterator_traits<Iterator>::pointer             pointer;
+class reverse_iterator
+{
+    protected :
+        Iterator iter;
+    public :
+        typedef Iterator iterator_type;
+        typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+        typedef typename iterator_traits<Iterator>::value_type value_type;
+        typedef typename iterator_traits<Iterator>::difference_type difference_type;
+        typedef typename iterator_traits<Iterator>::pointer pointer;
+        typedef typename iterator_traits<Iterator>::reference reference;
 };
 }
 #endif
