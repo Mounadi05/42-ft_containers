f7534966a614ec288ebe1b547b672984f2ccd196
Anass Mounadi <mounadi2015@gmail.com>
2023-03-17 01:23:49 +0100

    add some algo to folder tools

--- tools.hpp
+++ tools.hpp
@@ -1,6 +1,8 @@
 #ifndef TOOLS_HPP
 #define TOOLS_HPP
 
+namespace ft{
+
     template <class InputIterator1, class InputIterator2>
     bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
                                 InputIterator2 first2, InputIterator2 last2)
@@ -14,4 +16,18 @@
         return (first2!=last2);
     }
 
+    template <class T> 
+    const T& max (const T& a, const T& b) {return (a<b) ? b : a;}
+    
+    template <class InputIterator1, class InputIterator2>
+    bool equal ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2 )
+    {
+        while (first1!=last1) 
+        {
+            if (!(*first1 == *first2)) return false;
+            ++first1; ++first2;
+        }
+        return true;
+    }
+}
 #endif
