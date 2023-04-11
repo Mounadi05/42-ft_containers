ee128807a64334ae5d0488fb9201d84873a1c68d
Anass Mounadi <mounadi2015@gmail.com>
2023-04-11 01:31:16 +0000

    add binary_function

--- tools.hpp
+++ tools.hpp
@@ -49,5 +49,13 @@ namespace ft{
     {
         return (pair<T1,T2>(x,y) );
     }
+
+    template <class Arg1, class Arg2, class Result>
+    struct binary_function 
+    {
+        typedef Arg1 first_argument_type;
+        typedef Arg2 second_argument_type;
+        typedef Result result_type;
+    };
 }
 #endif
