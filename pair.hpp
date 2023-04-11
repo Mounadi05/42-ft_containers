a72d531b8ffa7d1da59475e7c8ee605ca6d795d2
Anass Mounadi <mounadi2015@gmail.com>
2023-04-11 05:34:38 +0000

    nothing

--- pair.hpp
+++ pair.hpp
@@ -3,28 +3,28 @@
 
 namespace ft{
 
-template <class T1, class T2>
-struct pair 
-{
-    typedef T1 first_type;
-    typedef T2 second_type;
-    T1 first;
-    T2 second;
-    pair();
-    pair(const T1& x, const T2& y):first(x),second(y){}
-    template<class U, class V> 
-    pair(const pair<U, V> &p):first(p.first),second(p.second){}
-    template<class U, class V> 
-    pair & operator=(const pair<U, V> &p)
+    template <class T1, class T2>
+    struct pair 
     {
-        if(this->first != p.first && this->second != p.second)
+        typedef T1 first_type;
+        typedef T2 second_type;
+        T1 first;
+        T2 second;
+        pair();
+        pair(const T1& x, const T2& y):first(x),second(y){}
+        template<class U, class V> 
+        pair(const pair<U, V> &p):first(p.first),second(p.second){}
+        template<class U, class V> 
+        pair & operator=(const pair<U, V> &p)
         {
-            this->fisrt = p.first;
-            this->second = p.second;
+            if(this->first != p.first && this->second != p.second)
+            {
+                this->fisrt = p.first;
+                this->second = p.second;
+            }
+            return *this;
         }
-        return *this;
-    }
-};
+    };
     template <class T1, class T2>
     bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
     { 
@@ -58,7 +58,7 @@ struct pair
     template <class T1, class T2>
     bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
     { 
-        return !(lhs <r hs); 
+        return !(lhs <rhs); 
     }
 
 }
