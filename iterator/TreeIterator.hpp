74fdc1b26e3cb008b91c08160a7bfcf664b857fc
Anass Mounadi <mounadi2015@gmail.com>
2023-05-21 02:21:00 +0100

    update iterator

--- iterator/TreeIterator.hpp
+++ iterator/TreeIterator.hpp
@@ -7,104 +7,104 @@
 
 namespace ft
 {
-	template <class Val>
-	class TreeIterator
-	{
-	public:
-		typedef std::bidirectional_iterator_tag iterator_category;
-		typedef typename ft::iterator_traits<Val *>::value_type value_type;
-		typedef typename ft::iterator_traits<Val *>::reference reference;
-		typedef typename ft::iterator_traits<Val *>::pointer pointer;
-		typedef const typename ft::iterator_traits<Val *>::reference const_reference;
-		typedef const typename ft::iterator_traits<Val *>::pointer const_pointer;
-		typedef typename ft::iterator_traits<Val *>::difference_type difference_type;
-		typedef std::size_t size_type;
-		typedef Node<typename ft::remove_const<Val>::type> *node_pointer;
+    template <class Val>
+    class TreeIterator
+    {
+    public:
+        typedef std::bidirectional_iterator_tag iterator_category;
+        typedef typename ft::iterator_traits<Val *>::value_type value_type;
+        typedef typename ft::iterator_traits<Val *>::reference reference;
+        typedef typename ft::iterator_traits<Val *>::pointer pointer;
+        typedef const typename ft::iterator_traits<Val *>::reference const_reference;
+        typedef const typename ft::iterator_traits<Val *>::pointer const_pointer;
+        typedef typename ft::iterator_traits<Val *>::difference_type difference_type;
+        typedef std::size_t size_type;
+        typedef Node<typename ft::remove_const<Val>::type> *node_pointer;
 
-	protected:
-		node_pointer root;
+    protected:
+        node_pointer root;
 
-	public:
-		TreeIterator() {}
-		TreeIterator(node_pointer ptr) : root(ptr) {}
-		TreeIterator(const TreeIterator<typename ft::remove_const<Val>::type> &other) { *this = other; }
-		TreeIterator &operator=(const TreeIterator<typename ft::remove_const<Val>::type> &other)
-		{
-			if (this != &other)
-				root = other.root;
-			return *this;
-		}
-		~TreeIterator() {}
-		reference operator*() { return *root->data; }
-		pointer operator->() { return &operator*(); }
-		const_reference operator*() const { return *(root->data); }
-		const_pointer operator->() const { return &operator*(); }
-		node_pointer getPointer() const { return root; }
-		TreeIterator &operator++()
-		{
-			if (root->isNil)
-				root = root->parent;
-			else if (!root->right->isNil)
-			{
-				root = root->right;
-				while (!root->left->isNil)
-					root = root->left;
-			}
-			else
-			{
-				node_pointer tmp = root->parent;
-				while (!tmp->isNil && root == tmp->right)
-				{
-					root = tmp;
-					tmp = tmp->parent;
-				}
-				root = tmp;
-			}
-			return *this;
-		}
+    public:
+        TreeIterator() {}
+        TreeIterator(node_pointer ptr) : root(ptr) {}
+        TreeIterator(const TreeIterator<typename ft::remove_const<Val>::type> &other) { *this = other; }
+        TreeIterator &operator=(const TreeIterator<typename ft::remove_const<Val>::type> &other)
+        {
+            if (this != &other)
+                root = other.root;
+            return *this;
+        }
+        ~TreeIterator() {}
+        reference operator*() { return *root->data; }
+        pointer operator->() { return &operator*(); }
+        const_reference operator*() const { return *(root->data); }
+        const_pointer operator->() const { return &operator*(); }
+        node_pointer getPointer() const { return root; }
+        TreeIterator &operator++()
+        {
+            if (root->isNil)
+                root = root->parent;
+            else if (!root->right->isNil)
+            {
+                root = root->right;
+                while (!root->left->isNil)
+                    root = root->left;
+            }
+            else
+            {
+                node_pointer tmp = root->parent;
+                while (!tmp->isNil && root == tmp->right)
+                {
+                    root = tmp;
+                    tmp = tmp->parent;
+                }
+                root = tmp;
+            }
+            return *this;
+        }
 
-		TreeIterator operator++(int)
-		{
-			TreeIterator<value_type> tmp(*this);
-			++(*this);
-			return tmp;
-		}
-		TreeIterator &operator--()
-		{
-			if (root->isNil)
-			{
-				root = root->parent;
-			}
-			else if (!root->isNil && !root->left->isNil)
-			{
-				root = root->left;
-				while (!root->right->isNil)
-					root = root->right;
-			}
-			else
-			{
-				node_pointer tmp = root->parent;
-				while (!tmp->isNil && root == tmp->left)
-				{
-					root = tmp;
-					tmp = tmp->parent;
-				}
-				root = tmp;
-			}
-			return *this;
-		}
+        TreeIterator operator++(int)
+        {
+            TreeIterator<value_type> tmp(*this);
+            ++(*this);
+            return tmp;
+        }
+        TreeIterator &operator--()
+        {
+            if (root->isNil)
+            {
+                root = root->parent;
+            }
+            else if (!root->isNil && !root->left->isNil)
+            {
+                root = root->left;
+                while (!root->right->isNil)
+                    root = root->right;
+            }
+            else
+            {
+                node_pointer tmp = root->parent;
+                while (!tmp->isNil && root == tmp->left)
+                {
+                    root = tmp;
+                    tmp = tmp->parent;
+                }
+                root = tmp;
+            }
+            return *this;
+        }
 
-		TreeIterator operator--(int)
-		{
-			TreeIterator<value_type> tmp(*this);
-			--(*this);
-			return tmp;
-		}
-		template <class T>
-		bool operator==(const TreeIterator<T> &other) { return root == other.root; }
-		template <class T>
-		bool operator!=(const TreeIterator<T> &other) { return root != other.root; }
-	};
+        TreeIterator operator--(int)
+        {
+            TreeIterator<value_type> tmp(*this);
+            --(*this);
+            return tmp;
+        }
+        template <class T>
+        bool operator==(const TreeIterator<T> &other) const { return root == other.root; }
+        template <class T>
+        bool operator!=(const TreeIterator<T> &other) const { return root != other.root; }
+    };
 }
 
 #endif
